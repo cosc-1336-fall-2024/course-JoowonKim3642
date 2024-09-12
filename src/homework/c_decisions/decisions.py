700335ae77049db0cd82891da1f467a841ba4403
JoowonKim3642 <joowon.kim@g.austincc.edu>
2024-09-12 04:29:58 +0000

    Homework 2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,17 @@
-#
+#get_options_ratio and get_faculty_rating
+
+def get_options_ratio(option, total):
+    ratio = option/total
+    return ratio
+
+def get_faculty_rating(ratio):
+    if ratio >= .9 and ratio <1:
+        return "Excellent"
+    elif ratio > .8:
+        return "Very Good"
+    elif ratio > .7:
+        return "Good"
+    elif ratio > .6:
+        return "Needs Improvement"
+    elif ratio > .0 and ratio < .59:
+        return "Unacceptable"
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,24 @@
-#
+#main get_options_ratio and get_faculty_rating
+
+import decisions
+
+def main():
+
+    option = int(input("Enter option: "))
+    total = int(input("Enter total: "))
+
+    result1 = decisions.get_options_ratio(option, total)
+    result2 = decisions.get_faculty_rating(result1)
+    print("Rating: ", result2)
+  
+main()
+
+#solving errors ver
+    #try:
+        #result1 = decisions.get_options_ratio(option, total)
+        #result2 = decisions.get_faculty_rating(result1)
+        #print("Rating: ", result2)
+    #except ZeroDivisionError:
+        #print("Total cannot be 0")
+#need to solve
+#if ratio is over or equals 1, result of get_faculty_rating is very good
