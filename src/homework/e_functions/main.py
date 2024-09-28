74b9187811b51a21a0c036f8d27530ac99ec91e4
JoowonKim3642 <joowon.kim@g.austincc.edu>
2024-09-28 19:47:26 +0000

    HW 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,18 @@
-#
+#main
+
+from value_return import get_hour, get_minutes, get_seconds
+import time
+
+def main():
+
+    epoch_seconds = 3800
+
+    hour = get_hour(epoch_seconds)
+    minute = get_minutes(epoch_seconds)
+    seconds = get_seconds(epoch_seconds)
+    
+    print(f"The time is {hour:02}:{minute:02}:{seconds:02}")
+
+    print(time.strftime('%Y.%m.%d - %H:%M:%S'),"UTC")
+
+main()
