8d1ea5703f877504c33a5895de4054230d9a6a27
JoowonKim3642 <joowon.kim@g.austincc.edu>
2024-11-03 04:58:40 +0000

    HW 7

--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,1 +1,37 @@
-#
+#main
+
+from lists import get_lowest_list_value, get_highest_list_value
+
+def main():
+    while True:
+        print("MENU\n1-Show the list low /high values\n2-Exit")
+
+        option = input("Select 1 or 2: ")
+
+        if option == "1":
+            numbers = []
+            while True:  
+                num = int(input("please enter a integer value: "))
+                numbers.append(num)
+
+                if len(numbers) > 2:
+                    answer = input("Do you want to enter another value? y/n ").upper()
+                    if answer == "Y":
+                        continue
+                    elif answer == "N":
+                        break
+
+            lowest = get_lowest_list_value(numbers)
+            highest = get_highest_list_value(numbers)
+            
+            print(f"The lowest number in the list is: {lowest}")
+            print(f"The highest number in the list is: {highest}")
+
+        elif option == "2":
+            print("Exit")
+            break
+
+        else:
+            print("Invalid option")
+
+main()
--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,1 +1,14 @@
-#
+#test
+
+import unittest
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value, get_highest_list_value
+
+class Test_Config(unittest.TestCase):
+
+    def test_lowest_list_value(self):
+        numbers = [8, 10, 1, 50, 20]
+        self.assertEqual(1, get_lowest_list_value(numbers))
+
+    def test_highest_list_value(self):
+        numbers = [8, 10, 1, 50, 20]
+        self.assertEqual(50, get_highest_list_value(numbers))
