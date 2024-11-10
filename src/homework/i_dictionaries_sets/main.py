ee27dd63ce9fb6f7712c0547793526e57b96de0b
JoowonKim3642 <joowon.kim@g.austincc.edu>
2024-11-10 07:25:06 +0000

    HW 8

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,40 +1,32 @@
 #main
 
-from dictionary import get_p_distance_matrix
+from dictionary import add_inventory, remove_inventory_widget
 
-def get_total_list():
-    num = int(input("how many list will enter? "))
-    index = 0
-    total_list = []
-
-    while(index < num):
-        input_list = input("please enter a comma-separated list: ")
-        s_list = input_list.split(",")
-        s_list = [value.strip() for value in s_list]
-        total_list.append(s_list)
-        index += 1
-
-    return total_list
-
+def menu():
+    print("Inventory Menu\n1-Add or Update Item\n2-Delete Item\n3-Exit")
 
 def main():
+    inventory = {}
+
     while True:
-        print("MENU\n1-Get p distance matrix\n2-Exit")
+        menu()
+        choice = input("Enter your selection: ")
 
-        option = input("Select 1 or 2: ")
-        result = []
-
-        if option == "1":
-            result0 = get_total_list()
-            result = get_p_distance_matrix(result0)
-            for row in result:
-                print(" ".join(f"{dist:.3f}" for dist in row))
-
-        elif option == "2":
+        if choice == '1':
+            widget = input("item name: ")
+            quantity = int(input("quantity: "))
+            add_inventory(inventory, widget, quantity)
+            print(f"Inventory updated: {inventory}\n")
+        
+        elif choice == '2':
+            widget = input("Enter item name you want to delete: ")
+            remove_inventory_widget(inventory, widget)
+            print(f"Inventory after deletion: {inventory}\n")
+        
+        elif choice == '3':
             print("Exit")
             break
-
+        
         else:
-            print("Invalid option")
-
+            print("Invalid value")
 main()
